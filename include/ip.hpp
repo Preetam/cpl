efb24d9165989a2d50ebccd491d1f06e80cb92e8
Preetam Jinka <pj@preet.am>
2015-04-16 00:56:24 -0400

    INET_ADDR6 support

--- include/ip.hpp
+++ include/ip.hpp
@@ -17,13 +17,43 @@ public:
 	IP(const std::string& address)
 	{
 		auto s = inet_pton(AF_INET, address.c_str(), &addr.v4_addr);
-		if (s < 0) {
-			throw std::invalid_argument("invalid address");
+		if (s == 1) {
+			// success
+			family = AF_INET;
+			return;
 		}
+
+		s = inet_pton(AF_INET6, address.c_str(), &addr.v6_addr);
+		if (s == 1) {
+			// success
+			family = AF_INET6;
+			return;
+		}
+		
+		throw std::invalid_argument("invalid address");
 	}
 
 	~IP() throw()
 	{
+	}
+
+	std::string string() const
+	{
+		if (family == AF_INET) {
+			// v4
+			char str[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, (void*)(&addr), str, INET_ADDRSTRLEN);
+			return std::string(str);
+		}
+
+		if (family == AF_INET6) {
+			// v6
+			char str[INET6_ADDRSTRLEN];
+			inet_ntop(AF_INET6, (void*)(&addr), str, INET6_ADDRSTRLEN);
+			return std::string(str);
+		}
+
+		return "";
 	}
 
 private:
@@ -34,16 +64,14 @@ private:
 	} addr;
 
 	friend std::ostream& operator << (std::ostream& os, const IP& ip);
-};
+}; // IP
 
-inline std::ostream& operator<<(std::ostream& os, const IP& ip) {
-	char str[INET_ADDRSTRLEN];
-	inet_ntop(AF_INET, (void*)(&ip.addr), str, INET_ADDRSTRLEN);
-	os << str;
+inline std::ostream& operator << (std::ostream& os, const IP& ip) {
+	os << ip.string();
 	return os;
 }
 
-}
-}
+} // net
+} // cpl
 
 #endif
--- test/ip.cc
+++ test/ip.cc
@@ -3,7 +3,14 @@
 
 int
 test_ip() {
+
+	// IPv4
 	auto ip = cpl::net::IP("127.0.0.1");
+	assert(ip.string() == "127.0.0.1");
+
+	// IPv6
+	ip = cpl::net::IP("dead:beef:cafe:0000:b4d:c0de:0000:0000");
+	assert(ip.string() == "dead:beef:cafe:0:b4d:c0de::");
 
 	return 0;
 }
