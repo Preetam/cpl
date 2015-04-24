28b9e77cc969ea530c11631a692f74a8dd8f5399
Preetam Jinka <pj@preet.am>
2015-04-24 11:15:02 -0400

    fix IPv6 socket binding

--- include/socket.hpp
+++ include/socket.hpp
@@ -43,7 +43,7 @@ public:
 		}
 	}
 
-	void bind(std::string& address, int port)
+	void bind(std::string address, int port)
 	{
 		local_address = IP(address);
 		local_port = port;
@@ -53,20 +53,21 @@ public:
 			throw std::runtime_error("couldn't create socket");
 		}
 
-		struct sockaddr addr;
-		addr.sa_family = local_address.family();
+		struct sockaddr_storage addr;
 
-		if (addr.sa_family == AF_INET) {
+		if (local_address.family() == AF_INET) {
 			auto addr_in = reinterpret_cast<struct sockaddr_in*>(&addr);
+			addr_in->sin_family = local_address.family();
 			addr_in->sin_addr = local_address.addr.v4_addr;
 			addr_in->sin_port = htons(port);
 		} else {
 			auto addr_in6 = reinterpret_cast<struct sockaddr_in6*>(&addr);
+			addr_in6->sin6_family = local_address.family();
 			addr_in6->sin6_addr = local_address.addr.v6_addr;
 			addr_in6->sin6_port = htons(port);
 		}
 
-		if (::bind(fd, &addr, sizeof addr) < 0) {
+		if (::bind(fd, reinterpret_cast<struct sockaddr*>(&addr), sizeof addr) < 0) {
 			throw std::runtime_error("failed to bind socket");
 		}
 	}
