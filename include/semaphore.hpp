1fe6338ca7b3c3a8fe0b965a840f543ec7e20f12
Preetam Jinka <pj@preet.am>
2015-09-30 00:19:25 -0400

    add Semaphore class

---
+++ include/semaphore.hpp
@@ -0,0 +1,39 @@
+#include <condition_variable>
+#include <mutex>
+
+namespace cpl
+{
+class Semaphore {
+public:
+    Semaphore(int n)
+    : _n(n)
+    {
+    }
+
+    inline
+    void acquire()
+    {
+        std::unique_lock<std::mutex> _(_m);
+        _cv.wait(_, []() {return _n > 0});
+        _n--;
+    }
+
+    inline
+    void release()
+    {
+        std::unique_lock<std::mutex> _(_m);
+        _n++;
+        _cv.notify_one();
+    }
+
+    // Disable copying.
+    Semaphore(const Semaphore&) = delete;
+    Semaphore& operator=(const Semaphore&) = delete;
+
+private:
+    int _n;
+    std::mutex  _m;
+    std::condition_variable _cv;
+
+}; // Semaphore
+} // cpl
