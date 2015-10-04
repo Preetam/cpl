ada86b9e6b5ba1316e5684a766d910bb067635b4
Preetam Jinka <pj@preet.am>
2015-10-04 13:59:35 -0400

    who put these spaces here

--- semaphore.hpp
+++ semaphore.hpp
@@ -7,35 +7,35 @@ namespace cpl
 {
 class Semaphore {
 public:
-    Semaphore(int n)
-    : _n(n)
-    {
-    }
+	Semaphore(int n)
+	: _n(n)
+	{
+	}
 
-    inline
-    void acquire()
-    {
-        std::unique_lock<std::mutex> _(_m);
-        _cv.wait(_, [this]() { return _n > 0; });
-        _n--;
-    }
+	inline
+	void acquire()
+	{
+		std::unique_lock<std::mutex> _(_m);
+		_cv.wait(_, [this]() { return _n > 0; });
+		_n--;
+	}
 
-    inline
-    void release()
-    {
-        std::unique_lock<std::mutex> _(_m);
-        _n++;
-        _cv.notify_one();
-    }
+	inline
+	void release()
+	{
+		std::unique_lock<std::mutex> _(_m);
+		_n++;
+		_cv.notify_one();
+	}
 
-    // Disable copying.
-    Semaphore(const Semaphore&) = delete;
-    Semaphore& operator =(const Semaphore&) = delete;
+	// Disable copying.
+	Semaphore(const Semaphore&) = delete;
+	Semaphore& operator =(const Semaphore&) = delete;
 
 private:
-    int _n;
-    std::mutex _m;
-    std::condition_variable _cv;
+	int _n;
+	std::mutex _m;
+	std::condition_variable _cv;
 
 }; // Semaphore
 } // cpl
